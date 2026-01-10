8284d0b4d3eae9b9e9aa66009e763efac2b73f39
askh-tamrakar <aksh.tamrakar666@gmail.com>
2026-01-10 14:57:36 +0530

    solved conflict

--- src/web/frontend/src/main.jsx
+++ src/web/frontend/src/main.jsx
@@ -9,11 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ClickSpark
-<<<<<<< HEAD:frontend/src/main.jsx
-        sparkColor='var(--muted)'
-=======
         sparkColor='var(--primary)'
->>>>>>> rps-implement:src/web/frontend/src/main.jsx
         sparkSize={10}
         sparkRadius={15}
         sparkCount={8}
